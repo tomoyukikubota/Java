91bea100660c21ed7d3a592c5ac50c9df9f64de0
tomoyukikubota <tomoyukikubota0119@gmail.com>
2021-09-04 13:56:40 +0900

    [Add]4章

---
+++ Java[基礎編]/Chapter04.java
@@ -0,0 +1,15 @@
+public class Chapter04 {
+
+  public static void main(String[] args) {
+
+    String name = "久保田智之";
+    final int LUKY_NUMBER = 9;
+    System.out.println(name);
+
+    name = "努力";
+    System.out.println(name);
+    System.out.println(LUKY_NUMBER);
+  }
+
+
+}

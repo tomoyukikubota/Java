fffcdcdf7bd4c9d1ae932b1cfe1c8d2460873933
tomoyukikubota <tomoyukikubota0119@gmail.com>
2021-09-04 15:03:19 +0900

    [Add]5章

---
+++ Java[基礎編]/Chapter05.java
@@ -0,0 +1,25 @@
+public class Chapter05 {
+  public static void main(String[] args) {
+    long long1 = 12345678910L;
+    System.out.println(long1);
+    
+    double double1 = 1234567.89;
+    System.out.println(double1);
+    
+    boolean boolean1 = true;
+    System.out.println(boolean1);
+
+    Float float1 = 0.123F;
+    System.out.println(float1);
+    
+    Integer integer1 = 1;
+    System.out.println(integer1);
+    
+    Character character1 = 'A';
+    System.out.println(character1);
+
+    int[] array = {10, 20, 30, 40, 50};
+    System.out.println(array[3]);
+  }
+  
+}
